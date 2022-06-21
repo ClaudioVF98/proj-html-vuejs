acebc7585134ae2c9647e6f72dba8dbab15217f0
Claudio <claudiovarrosi@gmail.com>
2022-06-21 17:18:13 +0200

    Lavoro completo

--- src/components/BakingOne.vue
+++ src/components/BakingOne.vue
@@ -12,12 +12,20 @@
             <div class="moments">
                 <!-- Immaginie e info Plans -->
                 <div class="plans">
-                    <img src="./images/corporate-bg.jpg" alt="">
+                    <div class="centered-info">
+                        <p>INTERNAL BAKERY</p>
+                        <h1>Corporate Events</h1>
+                        <button>Explore More</button>
+                    </div>
                 </div>
 
                 <!-- Immagine e info Wedding -->
                 <div class="wedding">
-                    <img src="./images/wedding-bg.jpg" alt="">
+                    <div class="centered-info">
+                        <p>PRIVATE DINING HALL</p>
+                        <h1>Weddings & Parties</h1>
+                        <button>Explore More</button>
+                    </div>
                 </div>
             </div>
         </div>
@@ -59,7 +67,95 @@ export default {
     .moments {
         display: flex;
         justify-content: space-evenly;
-        .plans, .wedding {
+        height: 700px;
+
+        .plans:hover .centered-info {
+            display: flex;
+        }
+
+        .plans:hover {
+            background-image: url("images/corporate-hover-bg.jpg");
+            background-size: cover;
+        }
+
+        .plans {
+            background-image: url("images/corporate-bg.jpg");
+            background-size: cover;
+
+            .centered-info * {
+                margin-bottom: 10px;
+            }
+
+            .centered-info {
+                flex-direction: column;
+                justify-content: center;
+                height: 100%;
+                color: white;
+                display: none;
+
+                button:hover {
+                    color: #55328b;
+                    background-color: #f0edf5;
+                }
+
+                button {
+                    width: 25%;
+                    align-self: center;
+                    padding: 3px;
+                    color: white;
+                    background-color: #55328b;
+                    border: 0;
+                    border-radius: 7px;
+                    cursor: pointer;
+                }
+            }
+        }
+
+
+        .wedding:hover .centered-info {
+            display: flex;
+        }
+
+        .wedding:hover {
+            background-image: url("images/wedding-hover-bg.jpg");
+            background-size: cover;
+        }
+
+        .wedding {
+            background-image: url("images/wedding-bg.jpg");
+            background-size: cover;
+
+            .centered-info * {
+                margin-bottom: 10px;
+            }
+
+            .centered-info {
+                flex-direction: column;
+                justify-content: center;
+                height: 100%;
+                color: white;
+                display: none;
+
+                button:hover {
+                    color: #55328b;
+                    background-color: #f0edf5;
+                }
+
+                button {
+                    width: 25%;
+                    align-self: center;
+                    padding: 3px;
+                    color: white;
+                    background-color: #55328b;
+                    border: 0;
+                    border-radius: 7px;
+                    cursor: pointer;
+                }
+            }
+        }
+
+        .plans,
+        .wedding {
             width: 40%;
 
             img {
